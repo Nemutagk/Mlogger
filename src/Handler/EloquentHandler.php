3f617527d9e5f77f044b20bb653488b5272e8919
Jose Alberto Cabrera <nemutagk@gmail.com>
2020-10-06 21:21:33 -0500

    Se agrega método para limpiar las keys antes de agregarlas

--- src/Handler/EloquentHandler.php
+++ src/Handler/EloquentHandler.php
@@ -9,11 +9,28 @@ class EloquentHandler extends AbstractProcessingHandler
 {
 	protected function write($config) : void {
 		$log = new Logger();
-		
+
+		$config = $this->clear_keys($config);
+
 		foreach($config as $key => $valor) {
 			$log->$key = $valor;
 		}
 
 		$log->save();
+	}
+
+	protected function clear_keys(array $array, array $search = ['.'], array $replace = ['-']) {
+		$newArray = [];
+
+		foreach($array as $key => $value) {
+			$key = str_replace($search, $replace, $key);
+
+			if (!is_array($value))
+				$newArray[$key] = $value;
+			else
+				$newArray[$key] = $this->clear_keys($value, $search, $replace);
+		}
+
+		return $newArray;
 	}
 }
